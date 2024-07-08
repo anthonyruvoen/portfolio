05422e764d0f605f0c948d84b8bc0ad3efcf2db7
Anthony <anthonyruvoen@gmail.com>
2024-07-08 15:15:09 +0200

    remove mail

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,52 +5,8 @@ const Contact = () => {
   return (
     <section id="contact">
       <h2>Contact</h2>
-      <p><i className="fa-regular fa-envelope"></i> a
-      {/* */}
-      n
-      {/* */}
-      t
-      {/* */}
-      h
-      {/* */}
-      o
-      {/* */}
-      n
-      {/* */}
-      y
-      {/* */}
-      r
-      {/* */}
-      u
-      {/* */}
-      v
-      {/* */}
-      o
-      {/* */}
-      e
-      {/* */}
-      n
-      {/* */}
-      @
-      {/* */}
-      g
-      {/* */}
-      m
-      {/* */}
-      a
-      {/* */}
-      i
-      {/* */}
-      l
-      {/* */}
-      .
-      {/* */}
-      c
-      {/* */}
-      o
-      {/* */}
-      m</p>
-      <p><i className="fa-brands fa-linkedin"></i>  https://www.linkedin.com/in/anthony-ruvoen/</p>
+      <p><i className="fa-regular fa-envelope"></i></p>
+      <p><i className="fa-brands fa-linkedin"></i></p>
     </section>
   );
 };
